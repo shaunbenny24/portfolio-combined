db3977864beab502d8cd0e40a70b961516caa4ea
shaunbenny24 <shaunchukkiriyan@gmail.com>
2026-06-03 12:32:52 +0530

    updated the project and skill upodate view

--- core/views.py
+++ core/views.py
@@ -4,6 +4,14 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Project, Skill, Contact
 from .forms import ProjectForm, SkillForm, ContactForm
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.shortcuts import redirect
+from core.models import Project
+from core.forms import ProjectForm
+
 
 
 # --- Page Views ---
@@ -30,7 +38,6 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context['contacts'] = Contact.objects.all().order_by('-created_at')
         context['project_form'] = ProjectForm()
         context['skill_form'] = SkillForm()
-        # For edit forms, pass a dict of bound forms keyed by pk
         context['project_edit_forms'] = {
             p.pk: ProjectForm(instance=p) for p in context['projects']
         }
@@ -52,15 +59,19 @@ class ProjectCreateView(LoginRequiredMixin, View):
         return redirect('/dashboard/')
 
 
-class ProjectUpdateView(LoginRequiredMixin, View):
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    form_class = ProjectForm
     login_url = '/auth/login/'
+    success_url = reverse_lazy('core:dashboard') 
 
-    def post(self, request, pk):
-        project = get_object_or_404(Project, pk=pk)
-        form = ProjectForm(request.POST, instance=project)
-        if form.is_valid():
-            form.save()
-        return redirect('/dashboard/')
+    def form_valid(self, form):
+        messages.success(self.request, f"Project '{form.instance.title}' updated successfully!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Failed to update project. Please check your inputs.")
+        return redirect(self.success_url)
 
 
 class ProjectDeleteView(LoginRequiredMixin, View):
@@ -81,16 +92,31 @@ class SkillCreateView(LoginRequiredMixin, View):
         if form.is_valid():
             form.save()
         return redirect('/dashboard/')
-
-
-class SkillUpdateView(LoginRequiredMixin, View):
+    
+class SkillUpdateView(LoginRequiredMixin,UpdateView):
+    model = Skill
+    form_class = SkillForm
     login_url = '/auth/login/'
+    success_url = reverse_lazy('core:dashboard')
 
-    def post(self, request, pk):
-        skill = get_object_or_404(Skill, pk=pk)
-        form = SkillForm(request.POST, instance=skill)
-        if form.is_valid():
-            form.save()
+
+    def form_valid(self, form):
+        messages.success(self.request, f"Skill '{form.instance.skill_name}' updated succesfully!")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "failed to updatew skill, Please check yoiur Input!.")
+        return redirect(self.success_url)
+    
+
+# class SkillUpdateView(LoginRequiredMixin, View):
+#     login_url = '/auth/login/'
+
+#     def post(self, request, pk):
+#         skill = get_object_or_404(Skill, pk=pk)
+#         form = SkillForm(request.POST, instance=skill)
+#         if form.is_valid():
+#             form.save()
 
 
 class SkillDeleteView(LoginRequiredMixin, View):
